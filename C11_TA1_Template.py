71d4fc23b95a12cebc13821fbda15a04056177b8
whjr2021 <91585088+whjr2021@users.noreply.github.com>
2021-11-19 12:22:05 +0530

    Add files via upload

---
+++ C11_TA1_Template.py
@@ -0,0 +1,18 @@
+# Import "time" module
+
+
+# Call the function "time()" from "time" module and store the result in "t1" variable
+
+print("T1 =", t1, "seconds")
+
+# Introduce a delay of 10 seconds.
+# Call the function "sleep()" from "time" module and pass 10 as the input into fuction
+
+
+# Call the function "time()" from "time" module and store the result in "t2" variable
+
+print("T2 =", t2, "seconds")
+
+# Calculate the elapsed time by finding difference between "t1" and "t2" and store the difference "elapsed_time"
+
+print("Elapsed time =", elapsed_time, "seconds")
